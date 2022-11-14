826b4221904739703770706ff9e1fcf7ab88b90c
Matthew Pogue <matthewpogue606@gmail.com>
2022-11-13 22:55:35 -0600

    Added the channel layers dictionary to the settings file

--- botb/settings.py
+++ botb/settings.py
@@ -132,6 +132,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 # Tailwind config items
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
