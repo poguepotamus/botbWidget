5c0e1aed75fbbcef678a12613e78c8c899744602
Matthew Pogue <matthewpogue606@gmail.com>
2022-11-14 00:16:42 -0600

    Updated some production settings

--- botb/settings.py
+++ botb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!(a28(=a)(%cswq=o1qdjv&m=-)n7h_$v#loki!#qvllf3o#8m'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-!(a28(=a)(%cswq=o1qdjv&m=-)n7h_$v#loki!#qvllf3o#8m'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'hobocutie.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
