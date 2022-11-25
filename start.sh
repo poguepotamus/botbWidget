8ec9b3cf95eed1d62bd4ccdbca467eba16d618a3
poguepotamus <matthewpogue606@gmail.com>
2022-11-25 23:31:38 +0000

    Updated launch script
    Builds tailwind css files
    Collects static files
    Prints user messages including widget url

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
+echo -e "\rBuilding tailwind files, please wait."
+python manage.py tailwind build
+
+echo -e "\nCollecting all static files to serve, please wait."
+python manage.py collectstatic --noinput
+
+echo -e "\nLaunching server, find widget at http://hobocutie.com/botb/widget\n"
 sudo -E /home/matthew/.local/bin/daphne -b 0.0.0.0 -p 80 botb.asgi:application
