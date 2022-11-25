1d592d3445c12bd88dc6979b1c95803e60d4c627
Matthew Pogue <matthewpogue606@gmail.com>
2022-11-25 14:38:26 -0600

    Added production static url to settings.py

--- botb/settings.py
+++ botb/settings.py
@@ -131,7 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'prod/static/'
+STATIC_URL = os.environ.get(
+    'DJANGO_STATIC_URL',
+    'prod/static/'
+)
 STATIC_ROOT = 'prod/static/'
 
 # Default primary key field type
