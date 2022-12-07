de1d2ba8940a1990ead96ff1282bac372df55827
poguepotamus <matthewpogue606@gmail.com>
2022-12-07 20:08:41 +0000

    Updated start script to check if redis server is running on production machine

--- start.sh
+++ start.sh
@@ -4,5 +4,8 @@ python manage.py tailwind build
 echo -e "\nCollecting all static files to serve, please wait."
 python manage.py collectstatic --noinput
 
-echo -e "\nLaunching server, find widget at http://hobocutie.com/botb/widget\n"
+echo -e "\nRunning redis on port 6379 for channels communication." 
+docker start ed9c3d466f2f
+
+echo -e "\nLaunching server, find widget at http://hobocutie.com/battleotbois2022/widget\n"
 sudo -E /home/matthew/.local/bin/daphne -b 0.0.0.0 -p 80 botb.asgi:application
